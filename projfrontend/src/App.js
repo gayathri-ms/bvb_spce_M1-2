4e3d444607366ce3b37b958a61f95065d4523672
rutuja-patil923 <backup923rutu@gmail.com>
2021-02-28 09:11:46 +0530

    frontend updated

--- projfrontend/src/App.js
+++ projfrontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/pages/Home'
-import About from './components/pages/About'
+import SignIn from './components/pages/SignIn'
 import SignUp from './components/pages/SignUp'
 import Help from './components/pages/Help'
 import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
@@ -16,7 +16,7 @@ const App = () => {
           <Navbar/>
           <Switch>
             <Route exact path="/"  component={Home}/>
-            <Route exact path="/About"  component={About}/>
+            <Route exact path="/SignIn"  component={SignIn}/>
             <Route exact path="/SignUp"  component={SignUp}/>
             <Route exact path="/Help"  component={Help}/>
           </Switch>
